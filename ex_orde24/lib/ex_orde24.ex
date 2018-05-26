b152cc019a8c0f158b042139188403d141003c14
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-05-26 22:53:49 +0900

    いっぺんに全リストを作るような無謀なことをやめた

--- ex_orde24/lib/ex_orde24.ex
+++ ex_orde24/lib/ex_orde24.ex
@@ -2,20 +2,27 @@ defmodule ExOrde24 do
   def solve(input) do
     [base, pos] = input |> String.split(",") |> Enum.map(&String.to_integer/1)
 
-    result =
-      base
-      |> gen()
-      |> Enum.at(pos - 1, "-")
-      |> String.downcase()
-
-    IO.puts "\n#{input} => #{result}"
-
-    result
+    base
+    |> gen_and_at(pos)
+    |> String.downcase()
   end
 
-  def gen(base) do
-    1..base-1
-    |> Enum.flat_map(&gen(&1, 1, base - 1, base))
+  def gen_and_at(base, pos) do
+    gen_and_at(base, 1, pos)
+  end
+
+  def gen_and_at(base, length, pos) when pos < 1 or base <= length do
+    "-"
+  end
+
+  def gen_and_at(base, length, pos) do
+    series = gen(length, 1, base - 1, base)
+    cond do
+      Enum.count(series) < pos ->
+        gen_and_at(base, length + 1, pos - Enum.count(series))
+      true ->
+        Enum.at(series, pos - 1)
+    end
   end
 
   def gen(1, min, max, base) do
